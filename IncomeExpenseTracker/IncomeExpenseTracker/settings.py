bd30445db9bec97d77492095b4fcf21bb79c4b95
Nehemiah Kemayah <ingeniouskemayah@gmail.com>
2025-01-03 09:44:21 +0000

    starters

--- IncomeExpenseTracker/IncomeExpenseTracker/settings.py
+++ IncomeExpenseTracker/IncomeExpenseTracker/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'IncomeExpenseTracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR/ 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
